8a9d952e591ff36803f54091c72cb41fc95a9597
Dmitry Yakimenko <detunized@gmail.com>
2017-10-17 18:35:28 +0200

    Generate random nonce

--- roboform.rb
+++ roboform.rb
@@ -3,8 +3,12 @@
 # Copyright (C) 2017 Dmitry Yakimenko (detunized@gmail.com).
 # Licensed under the terms of the MIT license. See LICENCE for details.
 
-require "yaml"
+require "base64"
 require "httparty"
+require "securerandom"
+require "yaml"
+
+DISABLE_RANDOM = true
 
 class Http
     include HTTParty
@@ -21,7 +25,18 @@ class Http
     end
 end
 
+def generate_nonce length = 16
+    if DISABLE_RANDOM
+        "-DeHRrZjC8DZ_0e8RGsisg"
+    else
+        b64 = Base64.urlsafe_encode64 SecureRandom.random_bytes length
+        b64.sub /\=*$/, ""
+    end
+end
+
 def login username, password, http
+    username = URI.escape username
+    nonce = generate_nonce
 end
 
 config = YAML.load_file "config.yaml"
