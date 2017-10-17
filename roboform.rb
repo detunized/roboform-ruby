b8bac2aaa7d813f5030f32c97461ff74af8b773d
Dmitry Yakimenko <detunized@gmail.com>
2017-10-17 16:38:44 +0200

    Minimal HTTP client

--- roboform.rb
+++ roboform.rb
@@ -4,5 +4,27 @@
 # Licensed under the terms of the MIT license. See LICENCE for details.
 
 require "yaml"
+require "httparty"
+
+class Http
+    include HTTParty
+
+    def initialize
+        @post_headers = {}
+    end
+
+    def post url, args, headers = {}
+        self.class.post url, {
+            body: args,
+            headers: @post_headers.merge(headers)
+        }
+    end
+end
+
+def login username, password, http
+end
 
 config = YAML.load_file "config.yaml"
+http = Http.new
+
+login config["username"], config["password"], http
