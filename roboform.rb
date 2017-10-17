909547f9fd8d3d0eda735a9e9b65d0e7be825b4c
Dmitry Yakimenko <detunized@gmail.com>
2017-10-18 01:57:33 +0200

    Hash password

--- roboform.rb
+++ roboform.rb
@@ -88,17 +88,35 @@ def parse_auth_info header
     }
 end
 
+def hash_password password, auth_info
+    if auth_info[:md5?]
+        password = Digest::MD5.digest password
+    end
+
+    OpenSSL::PKCS5.pbkdf2_hmac password,
+                               auth_info[:salt],
+                               auth_info[:iterations],
+                               32,
+                               "sha256"
+end
+
+def step2 username, password, nonce, auth_info, http
+    hashed_password = hash_password password, auth_info
+end
+
 def login username, password, http
     nonce = generate_nonce
 
     # Step 1
     response = step1 username, nonce, http
 
-    code = response.code
-    if code == 401
+    if response.code == 401
         auth_info = parse_auth_info response.headers["WWW-Authenticate"]
-    elsif code != 200
-        raise "Step 1: Network request failed with HTTP status #{code}"
+        response = step2 username, password, nonce, auth_info, http
+    end
+
+    if response.code != 200
+        raise "Step 1: Network request failed with HTTP status #{response.code}"
     end
 
     response.parsed_response
@@ -112,17 +130,28 @@ def check expression, message = ""
     raise message if !expression
 end
 
-def test_parse_auth_info
+def test_parse_auth_info config
     auth_info = parse_auth_info 'SibAuth sid="6Ag93Y02vihucO9IQl1fbg",data' +
                                 '="cj0tRGVIUnJaakM4RFpfMGU4UkdzaXNnTTItdGp' +
                                 'nZi02MG0tLUZCaExRMjZ0ZyxzPUErRnQ4VU02NzRP' +
                                 'Wk9PalVqWENkYnc9PSxpPTQwOTY="'
+
     check auth_info[:sid] == "6Ag93Y02vihucO9IQl1fbg"
     check auth_info[:data] == "r=-DeHRrZjC8DZ_0e8RGsisgM2-tjgf-60m--FBhLQ26tg,s=A+Ft8UM674OZOOjUjXCdbw==,i=4096"
     check auth_info[:nonce] == "-DeHRrZjC8DZ_0e8RGsisgM2-tjgf-60m--FBhLQ26tg"
     check auth_info[:salt] == d64("A+Ft8UM674OZOOjUjXCdbw==")
     check auth_info[:iterations] == 4096
     check auth_info[:md5?] == false
+end
+
+def test_hash_password config
+    hashed = hash_password config["password"], {
+        salt: d64("A+Ft8UM674OZOOjUjXCdbw=="),
+        iterations: 4096,
+        md5?: false
+    }
+
+    check hashed == d64("b+rd7TUt65+hdE7+lHCBPPWHjxbq6qs0y7zufYfqHto=")
 end
 
 #
@@ -133,6 +162,8 @@ config = YAML.load_file "config.yaml"
 http = Http.new
 
 # Poor man's tests
-test_parse_auth_info
+private_methods.grep(/^test_/).each do |i|
+    send i, config
+end
 
-login config["username"], config["password"], http
+#login config["username"], config["password"], http
